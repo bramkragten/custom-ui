64b9d58c86d4f26be622fb8a7dfdb07a834c0956
Bram Kragten <mail@bramkragten.nl>
2018-11-07 09:33:37 +0100

    Add migrate-card

---
+++ migrate-card/migrate-card.js
@@ -0,0 +1,62 @@
+import {
+  LitElement, html,
+} from 'https://unpkg.com/@polymer/lit-element@^0.5.2/lit-element.js?module';
+
+class MigrateCard extends LitElement {
+  static get properties() {
+    return {
+      hass: Object,
+      config: Object,
+    };
+  }
+
+  _render({ hass, config }) {
+    return html`
+    <style>
+    ha-icon {
+      display: flex;
+      margin: auto;
+    }
+    paper-button {
+      display: flex;
+      margin: auto;
+      text-align: center;
+    }
+    </style>
+    <ha-card on-click="${ev => this._migrate(hass)}">
+      <paper-button>
+      <div>
+        <ha-icon icon="mdi:playlist-edit" style="width: 40%; height: 40%; color: var(--primary-text-color);"></ha-icon>
+        <span>Migrate config</span>
+      </div>
+      </paper-button>
+    </ha-card>
+    `;
+  }
+
+  _migrate(hass) {
+    hass.callWS({ type: "lovelace/config/migrate"}).then(
+            (resp) => {
+                alert("Succesfully migrated!");
+                console.log('Message success!', resp);
+            },
+            (err) => {
+                alert("Error: " + err.message);
+                console.error('Message failed!', err);
+            }
+    );
+  }
+
+  setConfig(config) {
+    this.config = config;
+  }
+
+  // The height of your card. Home Assistant uses this to automatically
+  // distribute all cards over the available columns.
+  getCardSize() {
+    return 1;
+  }
+
+}
+
+customElements.define('migrate-card', MigrateCard);
